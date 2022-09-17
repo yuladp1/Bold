7c097f95d6ff61ae69e94f921df8fb0e92abfd1c
yuladp1 <yuladp1@gmail.com>
2022-09-17 21:18:36 +0200

    init commit

---
+++ template.js
@@ -0,0 +1,23 @@
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Product' },
+      { message: 'Services' },
+      { message: 'About' },
+      { message: 'Log in' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Product' },
+      { message: 'Services' },
+      { message: 'About' },
+      { message: 'Log in' }
+    ]
+  },
+})
